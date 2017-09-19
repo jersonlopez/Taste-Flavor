32baa374405b0fa6ae7d382a681915953c129473
jersonlopez <jersonlopez.1204@gmail.com>
2017-09-19 15:01:48 -0500

    inicio de la aplicacion

---
+++ Taste&Flavor/app/src/main/java/co/edu/udea/compumovil/gr04_20172/proyecto/Register.java
@@ -0,0 +1,33 @@
+package co.edu.udea.compumovil.gr04_20172.proyecto;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        btnRegister = (Button) findViewById(R.id.buttonSignup);
+        btnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSignup:
+                Intent intent = new Intent(Register.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(getApplicationContext(), "Registro Exitoso", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
---
+++ Taste&Flavor/app/src/main/java/co/edu/udea/compumovil/gr04_20172/proyecto/SplashScreen.java
@@ -0,0 +1,27 @@
+package co.edu.udea.compumovil.gr04_20172.proyecto;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    static public int TIME_TO_WAIT_SPLASH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },TIME_TO_WAIT_SPLASH );
+    }
+}
