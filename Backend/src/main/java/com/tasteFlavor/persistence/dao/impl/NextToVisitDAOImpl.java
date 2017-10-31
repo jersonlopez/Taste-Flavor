411f0a44eb701ea926ecf3f6eb00b3c61367c15d
jersonlopez <jersonlopez.1204@gmail.com>
2017-10-31 15:38:33 -0500

    Demo Proyecto

---
+++ Backend/src/main/java/com/tasteFlavor/persistence/dao/impl/NextToVisitDAOImpl.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tasteFlavor.persistence.dao.impl;
+
+import com.tasteFlavor.persistence.dao.NextToVisitDAO;
+
+/**
+ *
+ * @author Daniel
+ */
+public class NextToVisitDAOImpl implements NextToVisitDAO{
+    
+}
